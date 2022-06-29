4fd5b8f70352265c8ce2f6f42f90eb4a1d2ed267
hem_b <hemanthb93@gmail.com>
2022-06-29 17:15:22 +0530

    [Hemanth]:selects ratings greater than 3 and of particular produt id

--- ProductReviewsManagement/ProductReviewsManagement/Program.cs
+++ ProductReviewsManagement/ProductReviewsManagement/Program.cs
@@ -1,34 +1,49 @@
-﻿/* ------- ADDS DEFAULT DATA INTO PRODUCT REVIEWS LIST ------- */
+﻿Console.WriteLine("Enter your choice\n\t1 - To Fetch Top 3 High rated records\n\t" +
+    "2 - Retrieve Products with rating higher then 3");
+int choice = Convert.ToInt32(Console.ReadLine());
+ProductReviewsManagement.ReviewsManagement reviewsManagement = new ProductReviewsManagement.ReviewsManagement();
+/* ------- ADDS DEFAULT DATA INTO PRODUCT REVIEWS LIST ------- */
+    
+    List<ProductReviewsManagement.ProductReview> reviews = new List<ProductReviewsManagement.ProductReview>()
+    {
+        new ProductReviewsManagement.ProductReview() { ProductId = 1, UserId = 1, Rating = 2, Review = "Average", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 2, UserId = 2, Rating = 1.5, Review = "Bad", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 3, UserId = 3, Rating = 3, Review = "Nice", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 4, UserId = 4, Rating = 4.5, Review = "Good", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 5, UserId = 5, Rating = 5, Review = "Excelent", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 6, UserId = 3, Rating = 3, Review = "Nice", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 7, UserId = 6, Rating = 2, Review = "Average", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 8, UserId = 5, Rating = 1, Review = "Bad", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 9, UserId = 10, Rating = 4, Review = "Good", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 1, UserId = 23, Rating = 5, Review = "Excelent", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 11, UserId = 5, Rating = 4, Review = "Nice", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 12, UserId = 4, Rating = 1, Review = "Very Bad", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 13, UserId = 12, Rating = 5, Review = "Excelent", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 14, UserId =17, Rating = 2.5, Review = "Average", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 15, UserId = 10, Rating = 3, Review = "Nice", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 16, UserId = 8, Rating = 1, Review = "Very Bad", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 17, UserId = 18, Rating = 5, Review = "Excelent", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 18, UserId = 9, Rating = 4, Review = "Good", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 19, UserId = 10, Rating = 5, Review = "Nice", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 20, UserId = 7, Rating = 2, Review = "Average", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 21, UserId = 6, Rating = 1, Review = "Bad", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 22, UserId = 5, Rating = 1, Review = "Very Bad", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 23, UserId = 10, Rating = 4, Review = "Good", isLike = false },
+        new ProductReviewsManagement.ProductReview() { ProductId = 24, UserId = 8, Rating = 1.5, Review = "Bad", isLike = true },
+        new ProductReviewsManagement.ProductReview() { ProductId = 25, UserId = 12, Rating = 2, Review = "Average", isLike = false },
+    };
+    foreach (var item in reviews)
+    {
+        Console.WriteLine(item.ProductId +" - "+item.UserId+" - "+item.Rating);
+    }
+switch (choice)
+{
+    case 1:
+        reviewsManagement.GetHighReviews(reviews);
+        break;
+    case 2:
+        reviewsManagement.RatingsGreaterThanThree(reviews);
+        break;
 
-List<ProductReviewsManagement.ProductReview> reviews = new List<ProductReviewsManagement.ProductReview>()
-{
-    new ProductReviewsManagement.ProductReview() { ProductId = 1, UserId = 1, Rating = 2, Review = "Average", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 2, UserId = 2, Rating = 1.5, Review = "Bad", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 3, UserId = 3, Rating = 3, Review = "Nice", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 4, UserId = 4, Rating = 4.5, Review = "Good", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 5, UserId = 5, Rating = 5, Review = "Excelent", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 6, UserId = 3, Rating = 3, Review = "Nice", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 7, UserId = 6, Rating = 2, Review = "Average", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 8, UserId = 5, Rating = 1, Review = "Bad", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 5, UserId = 10, Rating = 4, Review = "Good", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 10, UserId = 23, Rating = 5, Review = "Excelent", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 11, UserId = 5, Rating = 4, Review = "Nice", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 12, UserId = 4, Rating = 1, Review = "Very Bad", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 13, UserId = 12, Rating = 5, Review = "Excelent", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 14, UserId =17, Rating = 2.5, Review = "Average", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 15, UserId = 10, Rating = 3, Review = "Nice", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 16, UserId = 8, Rating = 1, Review = "Very Bad", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 17, UserId = 18, Rating = 5, Review = "Excelent", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 18, UserId = 9, Rating = 4, Review = "Good", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 19, UserId = 10, Rating = 5, Review = "Nice", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 20, UserId = 7, Rating = 2, Review = "Average", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 21, UserId = 6, Rating = 1, Review = "Bad", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 22, UserId = 5, Rating = 1, Review = "Very Bad", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 23, UserId = 10, Rating = 4, Review = "Good", isLike = false },
-    new ProductReviewsManagement.ProductReview() { ProductId = 24, UserId = 8, Rating = 1.5, Review = "Bad", isLike = true },
-    new ProductReviewsManagement.ProductReview() { ProductId = 25, UserId = 12, Rating = 2, Review = "Average", isLike = false },
-};
-foreach (var item in reviews)
-{
-    Console.WriteLine(item.ProductId +" - "+item.UserId+" - "+item.Rating);
 }
+
---
+++ ProductReviewsManagement/ProductReviewsManagement/ReviewsManagement.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReviewsManagement
+{
+    public class ReviewsManagement
+    {
+        //UC-2 FETCHING TOP-3 HIGH RATED REVIEWS
+        public void GetHighReviews(List<ProductReview> productReviews)
+        {
+            var highRated = from review in productReviews orderby review.Rating descending select productReviews.Take(3).FirstOrDefault();
+            foreach(var review in highRated)
+            {
+                Console.WriteLine(review.ProductId+" | "+review.Rating);
+            }
+        }
+        //UC-3 FETCHING RECORDS WITH RATING > 3 AND PRODUCT IS 1 OR 4 OR 9
+        public void RatingsGreaterThanThree(List<ProductReview> productReviews)
+        {
+            var result = (from product in productReviews
+                          where product.Rating > 3 &&
+                          (product.ProductId == 1 || product.ProductId == 4 || product.ProductId == 9)
+                          select product);
+            Console.WriteLine("Records having ratings greater than 3 and who's product ID is 1 or 4 or 9 is ->");
+            foreach (var item in result)
+            {
+                Console.WriteLine("Product ID: " + item.ProductId + "\tUser ID: " + item.UserId + "\tRating: " + item.Rating + "\tReview: " + item.Review + "\tisLike: " + item.isLike);
+
+            }
+        }
+    }
+}
